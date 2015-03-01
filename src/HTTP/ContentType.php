34e469455bbc08099484ab696509b428954afdad
USAMI Kenta <tadsan@zonu.me>
2015-03-02 02:26:39 +0900

    Add Baguette/HTTP/ContentType

---
+++ src/HTTP/ContentType.php
@@ -0,0 +1,15 @@
+<?php
+namespace Baguette\HTTP;
+
+/**
+ * Interface of Response classes
+ *
+ * @package   Baguette\HTTP
+ * @author    USAMI Kenta <tadsan@zonu.me>
+ * @copyright 2015 USAMI Kenta
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ */
+final class ContentType
+{
+    const Application_OctetStream = 'application/octet-stream';
+}
